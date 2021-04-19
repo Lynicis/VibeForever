0ae80b122c6ea4f3ca2641f6e0a2258212a0e7cc
İsmail Emre Sırmalı <lynicis@protonmail.com>
2021-04-20 01:43:03 +0300

    create dev env

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 300,
+        height: 300,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile("file://" + path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
